b765ba88edfc7a991c639855a405113dbe0acbd4
Alan Quille <malanquille@gmail.com>
2020-11-28 20:24:24 +0000

    Added Week05 and modified, original approach was not working

---
+++ Week05/Server/restserver.py
@@ -0,0 +1,139 @@
+#!flask/bin/python
+from flask import Flask, jsonify,  request, abort, make_response
+from zstudentDAO import studentDAO
+
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='../')
+
+
+#@app.route('/cars', methods=['GET'])
+#def get_cars():
+ #   return jsonify( {'cars':cars})
+# curl -i http://localhost:5000/cars
+
+#@app.route('/cars/<string:reg>', methods =['GET'])
+#def get_car(reg):
+ #   foundCars = list(filter(lambda t : t['reg'] == reg , cars))
+#    if len(foundCars) == 0:
+#        return jsonify( { 'car' : '' }),204
+#    return jsonify( { 'car' : foundCars[0] })
+#curl -i http://localhost:5000/cars/test
+
+
+# instance of zstudentDAO object
+s = studentDAO
+
+@app.route('/create', methods=['POST'])
+def create():
+    if not request.json:
+        abort(400)
+    if not 'name' in request.json:
+        abort(400)
+    student={
+        "name":  request.json['name'],
+        "age": request.json['age']
+    }
+    values = (student["name"], student["age"])
+    s.create(values)
+    return jsonify( {'student':student }),201
+    #return jsonify(values),201
+
+
+@app.route('/read1', methods=['POST'])
+def read1():
+    if not request.json:
+        abort(400)
+    if not 'id' in request.json:
+        abort(400)
+    student={
+        "id":  request.json['id'],
+    }
+    values = student['id']
+    student["name"] = s.findByID(values)[1]
+    student["age"] = s.findByID(values)[2]
+    return jsonify(student),201
+    #return jsonify(values),201
+
+
+@app.route('/read2', methods=['GET'])
+def read2():
+    return jsonify(s.getAll()),201
+    #return jsonify(values),201
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    if not request.json:
+        abort(400)
+    if not 'name' in request.json:
+        abort(400)
+    student={
+        "name":  request.json['name'],
+        "age": request.json['age'],
+        "id" : request.json['id']
+    }
+    values = (student["name"], student["age"], student["id"])
+    s.update(values)
+    return jsonify( {'student':student }),201
+    #return jsonify(values),201
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    if not request.json:
+        abort(400)
+    if not 'id' in request.json:
+        abort(400)
+    student={
+        "id" : request.json['id']
+    }
+    values = student["id"]
+    s.delete(values)
+    return jsonify( {'student':student }),201
+    #return jsonify(values),201
+
+
+# sample test
+# curl -i -H "Content-Type:application/json" -X POST -d '{"reg":"12 D 1234","make":"Fiat","model":"Punto","price":3000}' http://localhost:5000/cars
+# for windows use this one
+# curl -i -H "Content-Type:application/json" -X POST -d "{\"reg\":\"12 D 1234\",\"make\":\"Fiat\",\"model\":\"Punto\",\"price\":3000}" http://localhost:5000/cars
+@app.route('/cars/<string:reg>', methods =['PUT'])
+def update_car(reg):
+    foundCars=list(filter(lambda t : t['reg'] ==reg, cars))
+    if len(foundCars) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'make' in request.json and type(request.json['make']) != str:
+        abort(400)
+    if 'model' in request.json and type(request.json['model']) is not str:
+        abort(400)
+    if 'price' in request.json and type(request.json['price']) is not int:
+        abort(400)
+    foundCars[0]['make']  = request.json.get('make', foundCars[0]['make'])
+    foundCars[0]['model'] =request.json.get('model', foundCars[0]['model'])
+    foundCars[0]['price'] =request.json.get('price', foundCars[0]['price'])
+    return jsonify( {'car':foundCars[0]})
+#curl -i -H "Content-Type:application/json" -X PUT -d '{"make":"Fiesta"}' http://localhost:5000/cars/181%20G%201234
+# for windows use this one
+#curl -i -H "Content-Type:application/json" -X PUT -d "{\"make\":\"Fiesta\"}" http://localhost:5000/cars/181%20G%201234
+
+@app.route('/cars/<string:reg>', methods =['DELETE'])
+def delete_car(reg):
+    foundCars = list(filter (lambda t : t['reg'] == reg, cars))
+    if len(foundCars) == 0:
+        abort(404)
+    cars.remove(foundCars[0])
+    return  jsonify( { 'result':True })
+
+@app.errorhandler(404)
+def not_found404(error):
+    return make_response( jsonify( {'error':'Not found' }), 404)
+
+@app.errorhandler(400)
+def not_found400(error):
+    return make_response( jsonify( {'error':'Bad Request' }), 400)
+
+
+if __name__ == '__main__' :
+    app.run(debug= True)
--- rest_server.py
+++ rest_server.py
@@ -1,121 +1,31 @@
-from flask import Flask, url_for , request, redirect, abort, jsonify, render_template
+from flask import Flask, url_for, request, redirect, abort, jsonify, render_template
 from zstudentDAO import studentDAO
 
-app =  Flask(__name__, static_url_path='', static_folder='staticpages')
-
-books=[
-    { "id":1, "Title":"Harry Potter", "Author": "JK", "Price":10},
-    { "id":2, "Title":"Some cook book", "Author": "Mr. Angry Man", "Price":20},
-    { "id":3, "Title":"Python made easy", "Author": "Some liar", "Price":15}
-]
-nextId=4
-
-#@app.route('/index.html')
-#def render_static():
- #   output = "Rest server!"
- #   return render_template('index.html', output=output)
-
-
-def create():
-    global nextId
-    if not request.json:
-        abort(400)
-    
-
-    book = {
-        "id": nextId,
-        "Title": request.json["Title"],
-        "Author": request.json["Author"],
-        "Price": request.json["Price"]
-    }
-    books.append(book)
-    nextId += 1
-    return jsonify(book)
-
-@app.route('/homepage.html')
-def render_static():
-    output = "Rest server!"
-    return render_template('homepage.html', output=output)
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='../')
 
 
 # instance of zstudentDAO object
 s = studentDAO
 
-#@app.route('/books', methods=['POST'])
-@app.route('/create.html')
-def render_static_2():
-   # output = str(jsonify(books))
-   # output = books
-    values = ("Alan", 30)
-    output = str(s.findByID(s.create(values)))
-    return render_template('create.html', output=output)
+@app.route('/cars', methods=['POST'])
+def create_car():
+    if not request.json:
+        abort(400)
+    if not 'reg' in request.json:
+        abort(400)
+    car={
+        "reg":  request.json['reg'],
+        "make": request.json['make'],
+        "model":request.json['model'],
+        "price":request.json['price']
+    }
+    cars.append(car)
+    return jsonify( {'car':car }),201
 
-#@app.route('/index.html')
-#def index():
- #   return "Hello"
 
-@app.route('/books/')
-def getAll():
-    return jsonify(books)
-
-@app.route('/books/<int:id>')
-def findByID(id):
-    foundBooks  = list(filter (lambda t : t["id"]==id, books))
-    if len(foundBooks) == 0:
-        return jsonify({}), 204
-    return jsonify(foundBooks[0])
-    #return "served by update with id " + str(id)
-
-# create
-# curl -X POST -H "content-type:application/json" -d "{\"Title\":\"test\", \"Author\":\"soome guy\", "\"Price\":123"}" http://127.0.0.1:5000/books
-#@app.route('/books', methods=['POST'])
-#def create():
-#    global nextId
-#    if not request.json:
-#        abort(400)
-    
-
-#    book = {
-#        "id": nextId,
-#        "Title": request.json["Title"],
-#        "Author": request.json["Author"],
-#        "Price": request.json["Price"]
-#    }
-#    books.append(book)
-#    nextId += 1
-#    return jsonify(book)
-
-# update
-#  curl -X PUT -H "content-type:application/json" -d "{\"Title\":\"new Title\", \"Author\":\"soome guy\", "\"Price\":999"}" http://127.0.0.1:5000/books/2
-@app.route('/books/<int:id>', methods=['PUT'])
-def update(id):
-    foundBooks  = list(filter (lambda t : t["id"]==id, books))
-    if len(foundBooks) == 0:
-        return jsonify({}), 400
-    
-    currentBook = foundBooks[0]
-    if 'Title' in request.json:
-        currentBook['Title'] = request.json['Title']
-
-    if 'Author' in request.json:
-        currentBook['Author'] = request.json['Author']
-
-    if 'Price' in request.json:
-        currentBook['Price'] = request.json['Price']
-    
-    return jsonify(currentBook)
-
-# delete
-# curl -X DELETE http://127.0.0.1:5000/books/1
-@app.route('/books/<int:id>', methods=['DELETE'])
-def delete(id):
-    foundBooks  = list(filter (lambda t : t["id"]==id, books))
-    if len(foundBooks) == 0:
-        return jsonify({}), 404
-    books.remove(foundBooks[0])
-
-    return jsonify({"done":True})
-
+print(cars)
 
 
 if __name__ == "__main__":
