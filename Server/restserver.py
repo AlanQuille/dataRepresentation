2570496c6b4167463b487446d317d02119167903
Alan Quille <malanquille@gmail.com>
2020-11-29 23:17:30 +0000

    Got logon functionality working.

--- Server/restserver.py
+++ Server/restserver.py
@@ -1,27 +1,108 @@
 #!flask/bin/python
-from flask import Flask, jsonify,  request, abort, make_response
+from flask import Flask, jsonify,  request, abort, make_response, session, abort, redirect, url_for, g
 from zstudentDAO import studentDAO
 
 app = Flask(__name__,
             static_url_path='',
             static_folder='../')
 
+app.secret_key = 'somesecretkeythatonlyishouldknow'
+
 s = studentDAO
+
+
+
+example = 0
+
+
+class User:
+    id = 0
+    def __init__(self, username, password):
+        #self.id = id
+        self.username = username
+        self.password = password
+        User.id += 1
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(username='root', password=''))
+print(users)
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+# change this using login or not depending
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = ''
+    password = ''
+    return_string = ''
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.json['username']
+        password = request.json['password']
+
+        username_list = []
+        password_list = []
+            
+        for user in users:
+            username_list.append(user.username)
+            password_list.append(user.password)
+
+        if(username in username_list and password in password_list):
+            session['user_id'] = user.id
+            return_string = "Success!"
+
+        else:
+            return_string = "Failure!"
+    
+    print(return_string)
+    return jsonify(return_string)
+
+@app.route('/verify', methods=['GET'])
+def verify():
+    if not g.user:
+        return jsonify("0")
+    else:
+        return jsonify("1")
+
+
+
+# logout
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return "Goodbye!"
+
+
+# create /verify which uses GET and ajax
+# pass logged_in_or_not of particular user
+# if logged_in_or_not=0, redirect using javascript
+# to login 
 
 @app.route('/create', methods=['POST'])
 def create():
+    #if not session.get('logged_in'):
     if not request.json:
         abort(400)
     if not 'name' in request.json:
         abort(400)
     student={
-        "name":  request.json['name'],
-        "age": request.json['age']
-    }
+            "name":  request.json['name'],
+            "age": request.json['age']
+            }
     values = (student["name"], student["age"])
     s.create(values)
     return jsonify( {'student':student }),201
-    #return jsonify(values),201
+
 
 
 @app.route('/read1', methods=['POST'])
@@ -87,48 +168,11 @@ def delete():
     return jsonify(student),201
     #return jsonify(values),201
 
+@app.route('/')
+def is_user_logged_in():
+    if not g.user:
+        return redirect("login.html")
+    return redirect("homepage.html")
 
-# sample test
-# curl -i -H "Content-Type:application/json" -X POST -d '{"reg":"12 D 1234","make":"Fiat","model":"Punto","price":3000}' http://localhost:5000/cars
-# for windows use this one
-# curl -i -H "Content-Type:application/json" -X POST -d "{\"reg\":\"12 D 1234\",\"make\":\"Fiat\",\"model\":\"Punto\",\"price\":3000}" http://localhost:5000/cars
-@app.route('/cars/<string:reg>', methods =['PUT'])
-def update_car(reg):
-    foundCars=list(filter(lambda t : t['reg'] ==reg, cars))
-    if len(foundCars) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'make' in request.json and type(request.json['make']) != str:
-        abort(400)
-    if 'model' in request.json and type(request.json['model']) is not str:
-        abort(400)
-    if 'price' in request.json and type(request.json['price']) is not int:
-        abort(400)
-    foundCars[0]['make']  = request.json.get('make', foundCars[0]['make'])
-    foundCars[0]['model'] =request.json.get('model', foundCars[0]['model'])
-    foundCars[0]['price'] =request.json.get('price', foundCars[0]['price'])
-    return jsonify( {'car':foundCars[0]})
-#curl -i -H "Content-Type:application/json" -X PUT -d '{"make":"Fiesta"}' http://localhost:5000/cars/181%20G%201234
-# for windows use this one
-#curl -i -H "Content-Type:application/json" -X PUT -d "{\"make\":\"Fiesta\"}" http://localhost:5000/cars/181%20G%201234
-
-@app.route('/cars/<string:reg>', methods =['DELETE'])
-def delete_car(reg):
-    foundCars = list(filter (lambda t : t['reg'] == reg, cars))
-    if len(foundCars) == 0:
-        abort(404)
-    cars.remove(foundCars[0])
-    return  jsonify( { 'result':True })
-
-@app.errorhandler(404)
-def not_found404(error):
-    return make_response( jsonify( {'error':'Not found' }), 404)
-
-@app.errorhandler(400)
-def not_found400(error):
-    return make_response( jsonify( {'error':'Bad Request' }), 400)
-
-
-if __name__ == '__main__' :
+if __name__ == '__main__':
     app.run(debug= True)
